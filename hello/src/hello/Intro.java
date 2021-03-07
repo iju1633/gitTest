c617de62e6f5c9ee25d0476711fd921540a57ead
82102 <82102@DESKTOP-FS1L2S6>
2021-03-07 13:55:50 +0900

    객체 지향 프로그램(이병정)

---
+++ hello/src/hello/Intro.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class Intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕하세요?\n저는 자바를 좋아합니다.\n저의 나이는 24입니다.");
+	}
+
+}
