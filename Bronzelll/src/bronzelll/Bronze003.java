2c026af937d6debc6477d83856e9167b02d932ff
82102 <82102@DESKTOP-FS1L2S6>
2021-03-12 22:43:31 +0900

    부호 : BigInteger에 대해 알아보았다. 0이랑 compareTo메서드 사용해서 부호 판단할 수 있고, long의 범위도 알 수 있었고, Scanner 객체 생성을 for문안에 하는 실수는 다시는 하지 않을 것이다.

---
+++ Bronzelll/src/bronzelll/Bronze003.java
@@ -0,0 +1,33 @@
+package bronzelll;
+
+import java.math.BigInteger;
+import java.util.*;
+
+public class Bronze003 {
+
+	public static void main(String[] args) { // 9223372036854775807가 unsigned long long 크기의 반에서 -1한 값임. 자바에서는 long에 해당!!
+											 // 런타임 에러! ( 두 수를 더하거나 음수의 경우 빼게 되면 long 범위를 넘어감! 이럴 땐 BigInteger(무한대수) 사용해야햠, 실수의 경우 BigDecimal도 존재!
+											 // Scanner sc = new Scanner(System.in); 이걸 for문 안에 두면 객체를 여러번 생성하는 것임.
+		String[] result = new String[3];
+		Scanner sc = new Scanner(System.in);
+		
+		for (int k = 0; k < 3; k++) {
+			int N = sc.nextInt();
+			BigInteger sum = BigInteger.ZERO;
+			for (int i = 0; i < N; i++) {
+				BigInteger a = sc.nextBigInteger();
+				sum = sum.add(a);
+			}
+			if (sum.compareTo(BigInteger.ZERO) == 0) {
+				result[k] = "0";
+			} else if (sum.compareTo(BigInteger.ZERO) == -1) {
+				result[k] = "-";
+			} else
+				result[k] = "+";
+		}
+		for (int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+
+}
