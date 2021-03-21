6ac4410b78d335052632df3797951b2737e9af93
82102 <82102@DESKTOP-FS1L2S6>
2021-03-21 13:51:29 +0900

    난수의 범위 확실히 알았음! 그냥은 0.0이상 1.0미만임!

--- hello/src/ch3class/Ex3Mini2.java
+++ hello/src/ch3class/Ex3Mini2.java
@@ -4,12 +4,12 @@ public class Ex3Mini2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] array1 = {"Clubs", "Diamonds", "Hearts", "Spades"};
-		String[] array2 = {"2", "3", "4", "5", "6" ,"7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
-		for(int i=0; i<5; i++) {
-			int index1 = (int)(Math.random()*4);
-			int index2 = (int)(Math.random()*13);
-			
+		String[] array1 = { "Clubs", "Diamonds", "Hearts", "Spades" };
+		String[] array2 = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+		for (int i = 0; i < 5; i++) {
+			int index1 = (int) (Math.random() * 4); // index의 값은 0, 1, 2, 3이 무작위로 나와야하는 것이므로 4를 곱하는 것이다
+			int index2 = (int) (Math.random() * 13); // index의 값은 0, 1,..., 11, 12이 무작위로 나와야하는 것이므로 13을 곱하는 것이다
+
 			System.out.print(array1[index1] + "의 " + array2[index2]);
 			System.out.println();
 		}
